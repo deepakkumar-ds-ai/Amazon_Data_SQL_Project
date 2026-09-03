c6df55ae953ebdf99261e013c01783a501606eee
Deepak kumar <deepakkumar.datascientist@gmail.com>
2026-09-03 08:05:24 +0530

    Problem - 15 is solved

--- Problems.sql
+++ Problems.sql
@@ -424,6 +424,36 @@ JOIN order_items as oi
 ON o.order_id = oi.order_id
 GROUP BY 1;
 
+/* 
+16. IDENTIFY customers into returning or new
+if the customer has done more than 5 return categorize them	as returning otherwise new
+Challenge: List customers id, name, total orders, total returns
+*/
+
+SELECT
+	full_name as customers,
+	total_orders,
+	total_return,
+CASE
+	WHEN total_return >5 THEN 'Returning_customers' ELSE 'New_customers'
+	END as cx_category
+FROM
+(SELECT
+	CONCAT(c.first_name, ' ', c.last_name) as full_name,
+	COUNT(o.order_id) as total_orders,
+	SUM(CASE WHEN o.order_status = 'Returned' THEN 1 ELSE 0 END) as total_return
+FROM orders as o
+JOIN
+customers as c
+ON
+c.customer_id = o.customer_id
+JOIN
+order_items as oi
+ON oi.order_id = o.order_id
+GROUP BY 1
+);
+
+
 
 
 
