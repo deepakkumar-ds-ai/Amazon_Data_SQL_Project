e90e52a387317bb4a37a9d9ae4def445d8696f81
Deepak kumar <deepakkumar.datascientist@gmail.com>
2026-09-07 16:39:28 +0530

    Problem - 19 is solved

--- Problems.sql
+++ Problems.sql
@@ -500,7 +500,78 @@ JOIN shippings as s
 ON s.order_id = o.order_id
 GROUP BY 1
 
+/*
+19. Top 10 product with highest decreasing revenue ratio compare to last year(2022) and current year(2023)
+Challenge: Return product_id, product_name, category_name, 2022 revenue and 2023 revenue decrease reatio and end Round the result
+Note: Decrease ratio = cr-ls/ls* 100 (cs = current_year ls = last_year)
+*/
 
+-- join o - oi - p
+-- filter 2022
+-- group by	p_id sum(total_sale)
+
+-- join o - oi - p
+-- filter 2023
+-- group by	p_id sum(total_sale)
+
+-- join 1 - 2
+
+WITH last_year_sale
+as
+(
+SELECT
+	p.product_id,
+	p.product_name,
+	SUM(oi.total_sale) as revenue
+FROM orders as o
+JOIN
+order_items as oi
+ON o.order_id = oi.order_id
+JOIN
+products as p 
+ON p.product_id = oi.product_id
+WHERE EXTRACT(YEAR FROM o.order_date) = 2022
+GROUP BY 1, 2
+),
+
+current_year_sale
+as
+(
+SELECT
+	p.product_id,
+	p.product_name,
+	SUM(oi.total_sale) as revenue
+FROM orders as o
+JOIN
+order_items as oi
+ON o.order_id = oi.order_id
+JOIN
+products as p 
+ON p.product_id = oi.product_id
+WHERE EXTRACT(YEAR FROM o.order_date) = 2023
+GROUP BY 1, 2
+)
+
+SELECT 
+	cys.product_id,
+	lys.revenue as last_year_revenue,
+	cys.revenue as current_year_revenue,
+	lys.revenue - cys.revenue as rev_diff,
+	ROUND((cys.revenue - lys.revenue)::numeric/lys.revenue::numeric * 100, 2) as revenue_dec_ratio
+FROM last_year_sale as lys
+JOIN
+current_year_sale as cys
+ON cys.product_id = lys.product_id
+WHERE lys.revenue > cys.revenue
+ORDER BY 5 DESC
+LIMIT 10
+
+
+/*
+20. Store procedure
+create a function as soon as product is sold the same quantity should reduced from inventory table
+after adding any sales records it should update the stock in the inventory table based on the product and quantiy purchased
+*/
 
 
 
